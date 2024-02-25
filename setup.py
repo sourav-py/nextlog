3bd15164dae50caae0c5d8696260c1f8edc6c4b0
Sourav Singh <imsrv2k@gmail.com>
2024-02-25 11:28:45 +0530

    Folder structure and lib meta data

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nextlog',
+    version='0.1.0',
+    description='Nextlog offers developers a customizable and versatile logging experience, allowing them to easily tailor log output formats, destinations, and verbosity levels to suit their specific needs.',
+    author='Sourav',
+    author_email='imsrv2k@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        # list of dependencies
+    ],
+)
