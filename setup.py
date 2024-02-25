cc864b755717c736ae59121e29310933839a46ab
Sourav Singh <imsrv2k@gmail.com>
2024-02-25 23:46:25 +0530

    update gitignore, lib 0 version details (development phase notification

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
+description = 'Project in development phase.'
+long_description = 'Documentation coming soon.'
+
 setup(
     name='nextlog',
-    version='0.1.0',
-    description='Nextlog offers developers a customizable and versatile logging experience, allowing them to easily tailor log output formats, destinations, and verbosity levels to suit their specific needs.',
+    version='0.0.0',
+    description=description,
+    long_description=long_description,
     author='Sourav',
     author_email='imsrv2k@gmail.com',
     packages=find_packages(),
