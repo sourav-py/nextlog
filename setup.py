1bb4195f1867f220542095c8473ab2c001e04b2d
Sourav Singh <imsrv2k@gmail.com>
2024-03-13 22:25:31 +0530

    doc: Add long description for pypi project page

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from setuptools import setup, find_packages
+from pathlib import Path
 
 description = 'Project in development phase.'
-long_description = 'Documentation coming soon.'
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='nextlog',
-    version='0.0.2',
+    version='1.0.3',
     description=description,
     long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Sourav',
     author_email='imsrv2k@gmail.com',
+    url="https://github.com/sourav-py/nextlog",
     packages=find_packages(),
     install_requires=[
         # list of dependencies
